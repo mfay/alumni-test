ed510b7b8d6591298cf674bc7869f5ceef303481
Mark Fay <fay.mark@gmail.com>
2023-03-01 14:19:14 -0500

    will this work?

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -7,7 +7,7 @@ use Illuminate\Support\ServiceProvider as BaseServiceProvider;
 
 class ServiceProvider extends BaseServiceProvider
 {
-    public function boot(): void
+    public function boot()
     {
         $this->loadRoutesFrom(__DIR__ . '/routes.php');
     }
