0bf17e8e6584a6ab66de7191a19f67f416fffcad
Mark Fay <fay.mark@gmail.com>
2023-03-01 14:45:05 -0500

    trying other things

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -10,5 +10,9 @@ class ServiceProvider extends BaseServiceProvider
     public function boot()
     {
         $this->loadRoutesFrom(__DIR__ . '/routes.php');
+
+        $this->app['router']->get('/alumni/test2', function () {
+            return "Please work";
+        });
     }
 }
